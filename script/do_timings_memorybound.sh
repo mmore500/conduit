d85ed1d811977560372704b170d2e01c27cd04a5
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-10 00:21:52 -0400

    Reorganize timing scripts

--- script/do_timings_memorybound.sh
+++ script/do_timings_memorybound.sh
@@ -4,8 +4,10 @@ export OMP_PROC_BIND=true
 export OMP_PLACES=threads
 export OMP_PROC_BIND=close
 
-export PP_NUM_UPDATES=1000
-for SYNCHRONOUS in 0 1; do
+export PP_USE_OMP=0
+
+export PP_NUM_UPDATES=10000
+for SYNCHRONOUS in 0; do
   export PP_SYNCHRONOUS=$SYNCHRONOUS
   OUT_FILE="synchronous=${SYNCHRONOUS}+ext=.csv"
   echo "Threads,Work,Load,Replicate,Time" > $OUT_FILE
@@ -13,14 +15,22 @@ for SYNCHRONOUS in 0 1; do
     for LOAD_PER in 1 2 4 8 16; do
       AMT_WORK=$(( $NUM_THREADS * $LOAD_PER ))
       for REP in {0..9}; do
+
         echo "NUM_THREADS: ${NUM_THREADS}"
         export OMP_NUM_THREADS=$NUM_THREADS
+
+        RESISTANCE=100
+        echo "RESISTANCE: ${RESISTANCE}"
+        export PP_RESISTANCE=$RESISTANCE
+
         echo "AMT_WORK: ${AMT_WORK}"
-        GRID_SIZE=$(( $AMT_WORK * 1000))
+        GRID_SIZE=$(( $AMT_WORK * 100))
         echo "GRID_SIZE: ${GRID_SIZE}"
         export PP_GRID_SIZE=$GRID_SIZE
+
         echo "LOAD_PER: ${LOAD_PER}"
         echo "REP: ${REP}"
+
         /usr/bin/time -f "%e" -o tmp \
           ./pipe-profile \
           > /dev/null 2>&1
