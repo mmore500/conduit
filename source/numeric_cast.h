b71522f864745a1e64b83456218d0ce51f86b147
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-10 15:00:29 -0400

    Sync std async threads after creation

---
+++ source/numeric_cast.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include <limits>
+
+// adapted from https://stackoverflow.com/a/49658950
+
+template<typename Dst, typename Src>
+inline Dst numeric_cast(Src value)
+{
+    typedef std::numeric_limits<Dst> DstLim;
+    typedef std::numeric_limits<Src> SrcLim;
+
+    const bool positive_overflow_possible = DstLim::max() < SrcLim::max();
+    const bool negative_overflow_possible =
+            SrcLim::is_signed
+            or
+            (DstLim::lowest() > SrcLim::lowest());
+
+    // unsigned <-- unsigned
+    if((not DstLim::is_signed) and (not SrcLim::is_signed)) {
+        if(positive_overflow_possible and (value > DstLim::max())) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": positive overflow"));
+        }
+    }
+    // unsigned <-- signed
+    else if((not DstLim::is_signed) and SrcLim::is_signed) {
+        if(positive_overflow_possible and (value > DstLim::max())) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": positive overflow"));
+        }
+        else if(negative_overflow_possible and (value < 0)) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": negative overflow"));
+        }
+
+    }
+    // signed <-- unsigned
+    else if(DstLim::is_signed and (not SrcLim::is_signed)) {
+        if(positive_overflow_possible and (value > DstLim::max())) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": positive overflow"));
+        }
+    }
+    // signed <-- signed
+    else if(DstLim::is_signed and SrcLim::is_signed) {
+        if(positive_overflow_possible and (value > DstLim::max())) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": positive overflow"));
+        } else if(negative_overflow_possible and (value < DstLim::lowest())) {
+            throw std::overflow_error(__PRETTY_FUNCTION__ +
+                                      std::string(": negative overflow"));
+        }
+    }
+
+    // limits have been checked, therefore safe to cast
+    return static_cast<Dst>(value);
+}
