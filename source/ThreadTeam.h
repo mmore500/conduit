33d5377e724906fac1a4aca1da4340ed5496ce05
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-11 14:21:20 -0400

    Refactor out ThreadTeam class

---
+++ source/ThreadTeam.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include <thread>
+#include <utility>
+
+#include "base/vector.h"
+
+class ThreadTeam {
+
+  emp::vector<std::thread> workers;
+
+public:
+
+  template <typename... Args>
+  void Add(Args&&... args) {
+    workers.emplace_back(std::forward<Args>(args)...);
+  }
+
+  void Join() {
+    std::for_each(
+      std::begin(workers),
+      std::end(workers),
+      [](auto & worker){ worker.join(); }
+    );
+    workers.clear();
+  }
+
+  size_t Size() const { return workers.size(); }
+
+};
