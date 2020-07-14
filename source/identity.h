7d87c847d06a49ecca60fad5161808378e7636fd
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-14 11:41:46 -0400

    Scrape identity implementation from stack overflow

---
+++ source/identity.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <utility>
+
+// TODO cpp20
+// adpated from https://codereview.stackexchange.com/q/134627
+static constexpr struct identity_t {
+  template<typename T>
+  constexpr decltype(auto) operator()(T&& t) const noexcept {
+    return std::forward<T>(t);
+  }
+} identity{};
