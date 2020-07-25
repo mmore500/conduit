cdb12ae36b4167c8a04b71370c3d8c662dc58523
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-25 18:41:38 -0400

    Refactor thread assignent into utils

---
+++ source/Mesh.h
@@ -0,0 +1,136 @@
+#pragma once
+
+#include <unordered_map>
+
+#include "Duct.h"
+
+#include "mesh_utils.h"
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+class Mesh {
+
+  using node_id_t = size_t;
+  using pipe_id_t = size_t;
+
+  mesh_t<T, N> mesh;
+
+  // pipe_id -> node_id
+  std::unordered_map<size_t, size_t> input_registry;
+  std::unordered_map<size_t, size_t> output_registry;
+
+  void InitializeRegistries() {
+    for (size_t node_id = 0; node_id < mesh.size(); ++node_id) {
+      auto & node = mesh[node_id];
+      auto & [inputs, outputs] = node;
+
+      for (auto & link : inputs) {
+        emp_assert(input_registry.count(link) == 0);
+        input_registry[link.GetPipeID()] = node_id;
+      }
+
+      for (auto & link : outputs) {
+        emp_assert(output_registry.count(link) == 0);
+        output_registry[link.GetPipeID()] = node_id;
+      }
+
+    }
+  }
+
+  void InitializeInterThreadPipes(
+    const std::function<thread_id_t(node_id_t)> thread_assignment
+  ) {
+
+    for (size_t node_id = 0; node_id < mesh.size(); ++node_id) {
+      auto & node = mesh[node_id];
+      auto & [inputs, outputs] = node;
+      const thread_id_t my_thread = thread_assignment(node_id);
+
+      for (auto & link : inputs) {
+        const node_id_t their_node = output_registry[link];
+        const thread_id_t their_thread = thread_assignment(their_node);
+
+        if (my_thread != their_thread) {
+          link.GetInput().template EmplaceDuct<
+            ThreadDuct<T, N>
+          >();
+        }
+      }
+
+    }
+
+  }
+
+  void InitializeInterProcPipes(
+    const std::function<proc_id_t(node_id_t)> proc_assignment
+  ) {
+
+    for (size_t node_id = 0; node_id < mesh.size(); ++node_id) {
+      auto & node = mesh[node_id];
+      auto & [inputs, outputs] = node;
+      const proc_id_t my_proc = proc_assignment(node_id);
+
+      for (auto & link : inputs) {
+        const node_id_t their_node = output_registry[link];
+        const proc_id_t their_proc = proc_assignment(their_node);
+
+        if (my_proc != their_proc) {
+          link.GetInput().template SplitDuct<
+            ProcessInletDuct<T, N>
+          >(
+            their_proc,
+            link.GetPipeID()
+          );
+        }
+      }
+
+      for (auto & link : outputs) {
+        const node_id_t their_node = input_registry[link];
+        const proc_id_t their_proc = proc_assignment(their_node);
+
+        if (my_proc != their_proc) {
+          link.GetOutput().template SplitDuct<
+            ProcessOutletDuct<T, N>
+          >(
+            their_proc,
+            link.GetPipeID()
+          );
+        }
+      }
+
+    }
+
+  }
+
+public:
+
+  using value_type = io_bundle_t<T, N>;
+
+  Mesh(
+    const mesh_t<T, N> & mesh_,
+    const std::function<thread_id_t(node_id_t)> thread_assignment,
+    const std::function<proc_id_t(node_id_t)> proc_assignment
+      =[](const auto & node_id){ return 0; }
+  )
+  : mesh(mesh_) {
+    InitializeRegistries();
+    InitializeInterThreadPipes(thread_assignment);
+    InitializeInterProcPipes(proc_assignment);
+  }
+
+  value_type & GetNode(const node_id_t node_id) { return mesh[node_id]; }
+
+  value_type & operator[](const node_id_t node_id) { return GetNode(node_id); }
+
+  typename mesh_t<T, N>::iterator begin() { return std::begin(mesh); }
+
+  typename mesh_t<T, N>::iterator end() { return std::end(mesh); }
+
+  typename mesh_t<T, N>::const_iterator begin() const {
+    return std::begin(mesh);
+  }
+
+  typename mesh_t<T, N>::const_iterator end() const {
+    return std::end(mesh);
+  }
+
+};
---
+++ source/mesh_utils.h
@@ -0,0 +1,149 @@
+#pragma once
+
+#include "Inlet.h"
+#include "Outlet.h"
+#include "pipe_utils.h"
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+class InputLink {
+  Outlet<T, N> input;
+  size_t pipe_id;
+
+public:
+
+  InputLink(
+    const Outlet<T, N>& input_,
+    const size_t pipe_id_
+  ) : input(input_)
+  , pipe_id(pipe_id_)
+  { ; }
+
+  operator Outlet<T, N>() const { return input; }
+
+  operator size_t() const { return pipe_id; }
+
+  Outlet<T, N> & GetInput() { return input; }
+
+  size_t GetPipeID() const { return pipe_id; }
+
+};
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+class OutputLink {
+  Inlet<T, N> output;
+  const size_t pipe_id;
+
+public:
+
+  OutputLink(
+    const Inlet<T, N>& output_,
+    const size_t pipe_id_
+  ) : output(output_)
+  , pipe_id(pipe_id_)
+  { ; }
+
+  operator Inlet<T, N>() const { return output; }
+
+  operator size_t() const { return pipe_id; }
+
+  Inlet<T, N> & GetOutput() { return output; }
+
+  size_t GetPipeID() const { return pipe_id; }
+
+};
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+struct io_bundle_t {
+  emp::vector<InputLink<T, N>> inputs;
+  emp::vector<OutputLink<T, N>> outputs;
+};
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+using mesh_t = emp::vector<
+  io_bundle_t<T, N>
+>;
+
+template<typename T, size_t N=DEFAULT_BUFFER>
+mesh_t<T, N> make_ring_mesh(const size_t cardinality) {
+
+  std::vector<OutputLink<T, N>> inlets;
+  std::vector<InputLink<T, N>> outlets;
+
+  for (size_t i = 0; i < cardinality; ++i) {
+    auto res = make_pipe<T, N>();
+    auto & [inlet, outlet] = res;
+    inlets.push_back({inlet, i});
+    outlets.push_back({outlet, i});
+  }
+
+  // before rotate
+  // outlets: 0 1 2 3  (inputs)
+  //          | | | |
+  // inlets:  0 1 2 3  (outputs)
+
+  std::rotate(
+    std::rbegin(outlets),
+    std::rbegin(outlets) + 1,
+    std::rend(outlets)
+  );
+
+  // after rotate
+  // outlets:   0 1 2 3  (inputs)
+  //           \ \ \ \
+  // inlets:    3 0 1 2  (outputs)
+
+  mesh_t<T, N> res;
+
+  for (size_t i = 0; i < cardinality; ++i) {
+    res.push_back(io_bundle_t<T, N>{
+      {outlets[i]},
+      {inlets[i]}
+    });
+  }
+
+  return res;
+
+}
+
+// TODO
+// * make_random_mesh
+//   * degree is argument
+// * make_toroidal_mesh (make ring mesh is special case of this)
+//   * dimension is argument
+// * make_grid_mesh (like toroidal mesh, but no wraparound)
+//   * dimension is argument
+// * make_complete_mesh
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_contiguously(
+  const size_t num_threads,
+  const size_t num_nodes
+) {
+  return [=](const auto & node_id){
+    return node_id * num_threads / num_nodes;
+  };
+};
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_round_robin(
+  const size_t num_threads
+) {
+  return [=](const auto & node_id){
+    return node_id % num_threads;
+  };
+};
+
+// TODO assign_randomly
+// make a vector of IDs, shuffle it, capture it,
+// then return result of indexing into it
+
+// TODO assign_chunkily (rename)
+// arrange into n-dimensional volume
+// then divvy into n-dimensional subcubes
+// (special case of assign_contiguously)
+
+// TODO make a util to get only values corresponding
+// to your thread's or your proc's indices
+// out of a container
+// maybe make a class MeshDelegator that divies everything up in constructor
+// then you can iterate through sub-vectors?
