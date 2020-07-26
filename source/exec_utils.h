ecac37492863b5cadb10bc8484ae69f2b3054ec3
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-25 22:45:53 -0400

    Add nproc utility

---
+++ source/exec_utils.h
@@ -0,0 +1,20 @@
+#include <cstdio>
+#include <iostream>
+#include <memory>
+#include <stdexcept>
+#include <string>
+#include <array>
+
+// adapted from https://stackoverflow.com/a/478960
+std::string exec(const char* cmd) {
+    std::array<char, 128> buffer;
+    std::string result;
+    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+    if (!pipe) {
+        throw std::runtime_error("popen() failed!");
+    }
+    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+        result += buffer.data();
+    }
+    return result;
+}
