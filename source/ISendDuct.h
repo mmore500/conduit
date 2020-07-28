73cfd6a512b3b3700052ce05ff55d03ba36268df
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-28 14:29:51 -0400

    Only mark request complete on success

--- source/ISendDuct.h
+++ source/ISendDuct.h
@@ -86,7 +86,7 @@ class ISendDuct {
       MPI_STATUS_IGNORE
     ));
 #ifndef NDEBUG
-    request_states[send_position] = false;
+    if(flag) request_states[send_position] = false;
 #endif
 
     if (flag) --pending;
