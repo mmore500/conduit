218c7ebda6c3ffa7a3f48cb2969be255d2f2c231
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-07-30 00:54:41 -0400

    Move assign_utils to own header
    Helps with levilization

---
+++ include/utility/assign_utils.h
@@ -0,0 +1,30 @@
+#pragma once
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_integrated() {
+  return [](const auto & node_id){ return 0; };
+};
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_segregated() {
+  return [](const auto & node_id){ return node_id; };
+};
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_contiguously(
+  const size_t num_threads,
+  const size_t num_nodes
+) {
+  return [=](const auto & node_id){
+    return node_id * num_threads / num_nodes;
+  };
+};
+
+template<typename RETURN_TYPE>
+std::function<RETURN_TYPE(size_t)> assign_round_robin(
+  const size_t num_threads
+) {
+  return [=](const auto & node_id){
+    return node_id % num_threads;
+  };
+};
