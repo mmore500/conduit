f18ffbd9d2a0cdd5d1daa4a7acf1b9a7b7f7f819
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-03 16:16:16 -0400

    Bugfix: use same timestamp per benchmark batch

---
+++ microbenchmarks/script/upload.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+VERSION=$(git describe --dirty --always)
+TIME=$(date +%s)
+NNODE=$([ -z "$SLURM_JOB_NUM_NODES" ] && echo 1 || echo $SLURM_JOB_NUM_NODES)
+NPROC=$(nproc)
+
+OSF_PATH="microbenchmarks/context=${CONTEXT}/nnode=${NNODE}+nproc=${NPROC}/time=${TIME}+version=${VERSION}"
+
+set -o xtrace
+
+for f in */*/*.json *.csv; do
+  osf -u mmore500.login@gmail.com -p 7jkgp upload $f $OSF_PATH/$f;
+done
