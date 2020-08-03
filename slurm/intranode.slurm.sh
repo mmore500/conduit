5559a9da127d5734b50952656d81601211e9d57b
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-03 12:35:48 -0400

    Draft slurm benchmarking scripts

---
+++ slurm/intranode.slurm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+########## Define Resources Needed with SBATCH Lines ##########
+#SBATCH --time=4:00:00
+#SBATCH --mem=6G
+#SBATCH --nodes=8
+#SBATCH --ntasks=8
+#SBATCH --job-name conduit-intranode
+#SBATCH --account=devolab
+#SBATCH --output="/mnt/home/mmore500/slurmlogs/slurm-%A.out"
+#SBATCH --mail-type=FAIL
+#SBATCH --mail-user=mmore500@msu.edu
+
+export CONTEXT=icer_intranode
+
+zsh <(curl -s https://raw.githubusercontent.com/mmore500/conduit/master/slurm/run.sh)
