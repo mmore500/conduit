7f8a37607758bfd00608b930f6fb7a9792d81bdb
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-07 12:59:21 -0400

    Implement, write test stubs for topology lib

---
+++ tests/topology/TopoNodeOutput.cc
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "topology/TopoNodeOutput.h"
+
+TEST_CASE("Test TopoNodeOutput") {
+
+  // TODO flesh out stub test
+  uit::TopoNodeOutput{0};
+
+}
---
+++ tests/topology/Topology.cc
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "topology/Topology.h"
+
+TEST_CASE("Test Topology") {
+
+  // TODO flesh out stub test
+  uit::Topology{};
+
+}
