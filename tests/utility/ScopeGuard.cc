6f4b951fed483e0e1203bfdc5bb3eea216c04fc3
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-07 18:18:58 -0400

    Implement and test ScopeGuard

---
+++ tests/utility/ScopeGuard.cc
@@ -0,0 +1,37 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "utility/ScopeGuard.h"
+
+TEST_CASE("Test ScopeGuard with default OnDestruct")
+{
+
+  size_t i{};
+  REQUIRE( i == 0 );
+
+  {
+  const uit::ScopeGuard guard{ [&](){++i;} };
+  REQUIRE( i == 1 );
+  }
+
+  REQUIRE( i == 1 );
+
+
+}
+
+TEST_CASE("Test ScopeGuard with custom OnDestruct")
+{
+
+  size_t i{};
+  REQUIRE( i == 0 );
+
+  {
+  const uit::ScopeGuard guard{ [&](){++i;}, [&](){--i;} };
+  REQUIRE( i == 1 );
+  }
+
+  REQUIRE( i == 0 );
+
+
+}
