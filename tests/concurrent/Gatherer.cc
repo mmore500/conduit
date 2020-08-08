7bed811b7f6afb212fdb3b8b7a63417de2343a8d
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-08 03:02:47 -0400

    Rewrite concurrent tests to use Catch

--- tests/concurrent/Gatherer.cc
+++ tests/concurrent/Gatherer.cc
@@ -1,33 +1,46 @@
 #include <thread>
+#include <set>
+#include <algorithm>
 
 #include "mpi.h"
 
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_DEFAULT_REPORTER "multiprocess"
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "../MultiprocessReporter.h"
+
+#include "base/vector.h"
+
+#include "distributed/mpi_utils.h"
+#include "distributed/MPIGuard.h"
 #include "concurrent/Gatherer.h"
 #include "utility/CircularIndex.h"
 #include "parallel/ThreadTeam.h"
 #include "polyfill/barrier.h"
 
-
 #include "distributed/mpi_utils.h"
 #include "parallel/thread_utils.h"
 #include "utility/numeric_cast.h"
+
+const uit::MPIGuard guard;
+
+constexpr size_t num_threads{ 2 };
 
 uit::Gatherer<int> gather(MPI_INT);
 
 void do_work() {
 
-  static std::barrier barrier{uit::numeric_cast<std::ptrdiff_t>(2)};
+  static std::barrier barrier{uit::numeric_cast<std::ptrdiff_t>(num_threads)};
 
   barrier.arrive_and_wait();
 
-  gather.Put(uit::get_thread_id());
+  gather.Put(uit::get_thread_id() + num_threads * uit::get_proc_id());
 
 }
 
-// TODO add actual test things
-int main(int argc, char* argv[]) {
 
-  MPI_Init(&argc, &argv);
+TEST_CASE("Test Gatherer") {
 
   uit::ThreadTeam team;
 
@@ -36,11 +49,19 @@ int main(int argc, char* argv[]) {
 
   team.Join();
 
-  auto res = gather.Gather();
+  const auto res = gather.Gather();
 
-  if (res) for (auto & val : *res) std::cout << "v: " << val << std::endl;
+  if (uit::is_root()) REQUIRE( res );
 
-  MPI_Finalize();
+  if (res) {
+    emp::vector<int> target(num_threads * uit::get_nprocs());
+    std::iota(std::begin(target), std::end(target), 0);
 
-  return 0;
+    REQUIRE(
+      std::set(std::begin(*res), std::end(*res))
+        == std::set(std::begin(target), std::end(target))
+    );
+
+  };
+
 }
