5ae91ceb6a3d01d371a154be5599ba8d9494ce40
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-08 03:14:55 -0400

    Fixup implementation selection for duct tests

--- tests/conduit/intra/PendingDuct.cc
+++ tests/conduit/intra/PendingDuct.cc
@@ -1,3 +1,8 @@
-#define IntraDuct PendingDuct
+#include "conduit/ImplSpec.h"
+#include "conduit/intra/PendingDuct.h"
+
+using ImplSel = uit::ImplSelector<
+  uit::PendingDuct
+>;
 
 #include "IntraDuct.h"
--- tests/conduit/thread/AtomicPendingDuct.cc
+++ tests/conduit/thread/AtomicPendingDuct.cc
@@ -1,3 +1,8 @@
-#define ThreadDuct AtomicPendingDuct
+#include "conduit/ImplSpec.h"
+#include "conduit/thread/AtomicPendingDuct.h"
+
+using ImplSel = uit::ImplSelector<
+  uit::AtomicPendingDuct
+>;
 
 #include "ThreadDuct.h"
