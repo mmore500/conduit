466997d4daf3e1fba0357f50e06dcd76d148e272
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-08 02:27:41 -0400

    Update ThreadDuct tests to track source changes

--- tests/conduit/thread/ThreadDuct.h
+++ tests/conduit/thread/ThreadDuct.h
@@ -13,36 +13,39 @@
 
 #include "utility/benchmark_utils.h"
 #include "distributed/mpi_utils.h"
-#include "conduit/pipe_utils.h"
+#include "conduit/Conduit.h"
+#include "conduit/ImplSpec.h"
 #include "utility/numeric_cast.h"
 #include "utility/safe_compare.h"
-#include "mesh/mesh_utils.h"
+#include "topology/RingTopologyFactory.h"
 #include "utility/math_utils.h"
 #include "utility/assign_utils.h"
 
 #define MSG_T int
 #define num_nodes 4
 
+using Spec = uit::ImplSpec<MSG_T>;
+
 uit::Gatherer<MSG_T> gatherer(MPI_INT);
 
-void do_work(uit::io_bundle_t<MSG_T> bundle, const size_t node_id) {
+void do_work(uit::MeshNode<Spec> node, const size_t node_id) {
 
-  bundle.outputs[0].GetOutput().MaybePut(node_id);
+  node.GetOutput(0).MaybePut(node_id);
 
   static std::latch sync_before{uit::numeric_cast<std::ptrdiff_t>(num_nodes)};
   sync_before.arrive_and_wait();
 
   for (size_t rep = 0; rep < 100; ++rep) {
-    bundle.outputs[0].GetOutput().MaybePut(node_id);
+    node.GetOutput(0).MaybePut(node_id);
     uit::do_not_optimize(
-      bundle.inputs[0].GetInput().GetCurrent()
+      node.GetInput(0).GetCurrent()
     );
   }
 
   static std::latch sync_after{uit::numeric_cast<std::ptrdiff_t>(num_nodes)};
   sync_after.arrive_and_wait();
 
-  const MSG_T res = bundle.inputs[0].GetInput().GetCurrent();
+  const MSG_T res = node.GetInput(0).GetCurrent();
 
   assert(
     uit::safe_equal(res, uit::circular_index(node_id, num_nodes, -1))
@@ -65,14 +68,13 @@ int main(int argc, char* argv[]) {
 
   uit::ThreadTeam team;
 
-  uit::Mesh mesh{
-    uit::make_ring_mesh<MSG_T>(num_nodes),
+  uit::Mesh<Spec> mesh{
+    uit::RingTopologyFactory{}(num_nodes),
     uit::AssignSegregated<uit::thread_id_t>{}
   };
 
-  for (size_t node_id = 0; node_id < mesh.GetSize(); ++node_id) {
-    auto & node = mesh[node_id];
-    team.Add([=](){ do_work(node, node_id); });
+  for (size_t i = 0; i < num_nodes; ++i) {
+    team.Add([=](){ do_work(mesh.GetSubmesh(i)[0], i); });
   }
 
   team.Join();
