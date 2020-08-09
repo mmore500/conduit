dc2962e9a7c63bb108b42b59123e3e2e21d8019a
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-08 20:49:13 -0400

    ProconTopologyFactory -> ProConTopologyFactory

---
+++ tests/topology/ProConTopologyFactory.cc
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "topology/ProConTopologyFactory.h"
+
+TEST_CASE("Test ProConTopologyFactory") {
+
+  // TODO flesh out stub test
+  uit::ProConTopologyFactory{}(100);
+
+}
