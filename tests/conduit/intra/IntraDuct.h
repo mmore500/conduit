b7796ff424e39dd8dc365b4d4bcc0cb90bce2c2d
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-09 16:03:08 -0400

    Add Duct message validity tests

--- tests/conduit/intra/IntraDuct.h
+++ tests/conduit/intra/IntraDuct.h
@@ -1,6 +1,7 @@
 #include <thread>
 #include <unordered_set>
 #include <cassert>
+#include <ratio>
 
 #include "mpi.h"
 
@@ -15,7 +16,6 @@
 #include "mesh/Mesh.h"
 
 #include "distributed/MPIGuard.h"
-#include "conduit/ImplSpec.h"
 #include "utility/numeric_cast.h"
 #include "utility/safe_compare.h"
 #include "topology/RingTopologyFactory.h"
@@ -29,7 +29,7 @@ using Spec = uit::ImplSpec<MSG_T, DEFAULT_BUFFER, ImplSel>;
 
 const uit::MPIGuard guard;
 
-TEST_CASE("Test IntraDuct") {
+TEST_CASE("Test IntraDuct Connectivity") {
 
   uit::Mesh<Spec> mesh{ uit::RingTopologyFactory{}(num_nodes) };
 
@@ -42,6 +42,30 @@ TEST_CASE("Test IntraDuct") {
     REQUIRE(
       uit::safe_equal(res, uit::circular_index(node.GetNodeID(), num_nodes, -1))
     );
+  }
+
+}
+
+TEST_CASE("Test IntraDuct Validity") {
+
+  uit::Mesh<Spec> mesh{ uit::RingTopologyFactory{}(num_nodes) };
+
+  std::unordered_map<size_t, MSG_T> last;
+  for (MSG_T msg = 0; msg < 10 * std::kilo{}.num; ++msg) {
+    for (auto & node : mesh.GetSubmesh()) {
+      node.GetOutput(0).MaybePut(msg);
+      const MSG_T current = node.GetInput(0).GetCurrent();
+      REQUIRE( current >= 0 );
+      REQUIRE( last[node.GetNodeID()] <= current );
+      last[node.GetNodeID()] = current;
+    }
+  }
+
+  for (size_t i = 0; i < 10 * std::kilo{}.num; ++i) {
+    for (auto & node : mesh.GetSubmesh()) {
+      REQUIRE( node.GetInput(0).GetCurrent() >= 0 );
+      REQUIRE( node.GetInput(0).GetCurrent() == node.GetInput(0).GetCurrent() );
+    }
   }
 
 }
