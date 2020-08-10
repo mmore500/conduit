2f797d173d2c50382e6afb71bdd4e011dfd1d37e
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-10 15:27:04 -0400

    Add some tests for topology factories

--- tests/topology/DyadicTopologyFactory.cc
+++ tests/topology/DyadicTopologyFactory.cc
@@ -9,4 +9,13 @@ TEST_CASE("Test DyadicTopologyFactory") {
   // TODO flesh out stub test
   uit::DyadicTopologyFactory{}(100);
 
+  for (size_t i = 0; i < 150; ++i) {
+    REQUIRE( uit::DyadicTopologyFactory{}(i).size() == i );
+  }
+
+  for (const auto& node : uit::DyadicTopologyFactory{}(100)) {
+    REQUIRE( node.GetNumInputs() == 1);
+    REQUIRE( node.GetNumOutputs() == 1);
+  }
+
 }
--- tests/topology/LoopTopologyFactory.cc
+++ tests/topology/LoopTopologyFactory.cc
@@ -9,4 +9,13 @@ TEST_CASE("Test LoopTopologyFactory") {
   // TODO flesh out stub test
   uit::LoopTopologyFactory{}(100);
 
+  for (size_t i = 0; i < 150; ++i) {
+    REQUIRE( uit::LoopTopologyFactory{}(i).size() == i );
+  }
+
+  for (const auto& node : uit::LoopTopologyFactory{}(100)) {
+    REQUIRE( node.GetNumInputs() == 1);
+    REQUIRE( node.GetNumOutputs() == 1);
+  }
+
 }
