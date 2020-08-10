ae386afe60589bbb3c916f2706066aae79d98efb
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-10 15:27:04 -0400

    Add some tests for topology factories

--- tests/topology/ProConTopologyFactory.cc
+++ tests/topology/ProConTopologyFactory.cc
@@ -9,4 +9,13 @@ TEST_CASE("Test ProConTopologyFactory") {
   // TODO flesh out stub test
   uit::ProConTopologyFactory{}(100);
 
+  for (size_t i = 0; i < 150; ++i) {
+    REQUIRE( uit::ProConTopologyFactory{}(i).size() == i );
+  }
+
+  for (const auto& node : uit::ProConTopologyFactory{}(100)) {
+    REQUIRE( node.GetNumInputs() + node.GetNumOutputs() == 1);
+    REQUIRE( node.HasInputs() != node.HasOutputs() );
+  }
+
 }
--- tests/topology/RingTopologyFactory.cc
+++ tests/topology/RingTopologyFactory.cc
@@ -9,4 +9,14 @@ TEST_CASE("Test RingTopologyFactory") {
   // TODO flesh out stub test
   uit::RingTopologyFactory{}(100);
 
+  for (size_t i = 0; i < 150; ++i) {
+    REQUIRE( uit::RingTopologyFactory{}(i).size() == i );
+  }
+
+  for (const auto& node : uit::RingTopologyFactory{}(100)) {
+    REQUIRE( node.GetNumInputs() == 1);
+    REQUIRE( node.GetNumOutputs() == 1);
+  }
+
+
 }
