75060d659dbe749d6229bc70d779c324dbfe0d59
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-12 23:14:33 -0400

    Suppress JSON output for non-root procs

--- microbenchmarks/conduit/proc/ProcDuct.h
+++ microbenchmarks/conduit/proc/ProcDuct.h
@@ -14,7 +14,11 @@ const uit::ScopeGuard register_benchmarks( [](){ range.item<1>(); } );
 int main(int argc, char** argv) {
 
   // suppress json output for non-root procs
-  benchmark::Initialize(&argc, argv);
+  int one{1};
+  benchmark::Initialize(
+    uit::is_root() ? &argc : &one,
+    argv
+  );
 
   benchmark::RunSpecifiedBenchmarks();
 
