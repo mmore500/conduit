b0f561126bd6c5d9d2b901f92c522d26f530d6cb
rodsan0 <rodsan0@users.noreply.github.com>
2020-08-14 22:17:44 -0400

    Reimplement Topology as a class
    * Implement a const interface for reading from Topology as if it were a
    std::vector.
    
    * Provide methods for printing information about the Topology in a
    NetworkX compatible way.
    
    * Start moving stuff MeshTopology into Topology

--- include/topology/Topology.h
+++ include/topology/Topology.h
@@ -1,9 +1,87 @@
 #pragma once
 
+#include <iostream>
+#include <sstream>
+#include <string>
+#include <utility>
+#include <iterator>
+#include <set>
+#include <unordered_map>
+
 #include "TopoNode.h"
 
 namespace uit {
 
-using Topology = emp::vector<TopoNode>;
+class Topology {
+    using node_id_t = size_t;
+    using edge_id_t = size_t;
+
+    emp::vector<TopoNode> topology;
+
+    // ordered by edge_id
+    std::set<edge_id_t> edge_registry;
+    // edge_id -> node_id
+    std::unordered_map<edge_id_t, node_id_t> input_registry;
+    std::unordered_map<edge_id_t, node_id_t> output_registry;
+
+    void InitializeRegistries(const uit::Topology& topology) {
+        for (node_id_t node_id = 0; node_id < topology.size(); ++node_id) {
+            const uit::TopoNode& topo_node = topology[node_id];
+            RegisterNodeInputs(node_id, topo_node);
+            RegisterNodeOutputs(node_id, topo_node);
+        }
+    }
+
+    void RegisterNodeInputs(const node_id_t node_id, const uit::TopoNode& topo_node) {
+        for (const uit::TopoNodeInput& input : topo_node.GetInputs()) {
+            emp_assert(input_registry.count(input.GetEdgeID()) == 0);
+            edge_registry.insert(input.GetEdgeID());
+            input_registry[input.GetEdgeID()] = node_id;
+        }
+    }
+
+    void RegisterNodeOutputs(const node_id_t node_id, const uit::TopoNode& topo_node) {
+        for (const uit::TopoNodeOutput& output : topo_node.GetOutputs()) {
+            emp_assert(output_registry.count(output.GetEdgeID()) == 0);
+            edge_registry.insert(output.GetEdgeID());
+            output_registry[output.GetEdgeID()] = node_id;
+        }
+    }
+
+public:
+    Topology() = default;
+    Topology(const size_t size) : topology(size) { ; }
+    Topology(const std::istream& is) {
+        for ()
+     }
+
+    emp::vector<TopoNode>::const_iterator begin() const noexcept { return topology.begin(); }
+    emp::vector<TopoNode>::const_iterator end() const noexcept { return topology.end(); }
+
+    emp::vector<TopoNode>::const_iterator cbegin() const noexcept { return topology.cbegin(); }
+    emp::vector<TopoNode>::const_iterator cend() const noexcept { return topology.cend(); }
+
+    void push_back(const TopoNode& node) { topology.push_back(node); }
+    void push_back(TopoNode&& node) { topology.push_back(std::move(node)); }
+
+    template <typename... Args>
+    void emplace_back(Args&&... args) { topology.emplace_back(args); }
+
+    size_t GetSize() const noexcept { return topology.size(); }
+
+    const TopoNode& operator[](size_t n) const { return topology[n]; }
+
+    void print(const Topology& topo, std::ostream& os = std::cout) const noexcept {
+        for (size_t i = 0; i < topo.size(); ++i) {
+            os << i << " " << topo[i] << std::endl;
+        }
+    }
+
+    std::string ToString() const noexcept {
+        std::ostringstream oss;
+        print(*this, oss);
+        return oss.str();
+    }
+};
 
 }
