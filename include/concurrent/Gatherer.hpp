caf131ce2258e1cd5d906f005cb55b3bdf0e8b19
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-15 10:43:02 -0400

    Write docstrings for Gatherer

--- include/concurrent/Gatherer.hpp
+++ include/concurrent/Gatherer.hpp
@@ -12,18 +12,39 @@
 
 namespace uit {
 
+/**
+ * Gather data items from across threads and processes.
+ *
+ * Uses a thread-safe container to record values from different threads within a
+ * process. Then uses a MPI `gatherv` call to collect values from across
+ * processes. Arbitrary numbers of data items may be contributed from each
+ * thread and from each process.
+ *
+ * @tparam T Value type to be gathered.
+ */
 template<typename T>
 class Gatherer {
 
-  emp::vector<T> tokens;
+  /// Data items submitted from threads in the executing process.
+  emp::vector<T> items;
+  /// Enforces thread-safe access to @items.
   std::mutex mutex;
 
+  /// MPI datatype corresponding to @T.
+  // TODO use template metaprogramming to automatically deduce this
   MPI_Datatype mpi_type;
+  /// Set of MPI processes to gather values from.
   MPI_Comm comm;
 
+  /**
+   * Gather counts of data items from participating processes.
+   *
+   * @param root The ID of the process to gather to.
+   * @returns Data item count from each participating process.
+   */
   emp::vector<int> GatherCounts(const int root) {
 
-    const int count = tokens.size();
+    const int count = items.size();
     emp::vector<int> res(get_nprocs());
 
     verify(MPI_Gather(
@@ -43,6 +64,10 @@ class Gatherer {
 
 public:
 
+  /**
+   * @param mpi_type_ MPI datatype corresponding to @T.
+   * @param comm_ Set of MPI processes to gather values from.
+   */
   Gatherer(
     MPI_Datatype mpi_type_,
     MPI_Comm comm_=MPI_COMM_WORLD
@@ -51,46 +76,60 @@ public:
   , comm(comm_)
   { ; }
 
-  void Put(const T& token) {
+  /**
+   * Add a data item to the collection that will be gathered from.
+   *
+   * @param item The data item to store.
+   * @note This call is thread-safe.
+   */
+  void Put(const T& item) {
     const std::lock_guard guard(mutex);
-    tokens.push_back(token);
+    items.push_back(item);
   }
 
-  std::optional<emp::vector<T>> Gather(
-    const int root=0
-  ) {
+  /**
+   * Gather put data items from across threads and processes.
+   *
+   * @param root The process ID to gather to.
+   * @returns Gathered data items if root process, otherwise std::nullopt.
+   * @note All processes within @comm must make this call simultaneously.
+   * @note Only a single thread from each process should make this call.
+   */
+  std::optional<emp::vector<T>> Gather(const int root=0) {
     const std::lock_guard guard(mutex);
 
     const emp::vector<int> counts = GatherCounts(root);
 
+    // calculate where each processes' contribution should be placed
     emp::vector<int> displacements{0};
-
     std::partial_sum(
       std::begin(counts),
       std::end(counts),
       std::back_inserter(displacements)
     );
 
-    emp::vector<T> res(
-      std::accumulate(
-        std::begin(counts),
-        std::end(counts),
-        0
-      )
+    // initialize buffer to hold contributed items from all processes
+    const size_t num_items = std::accumulate(
+      std::begin(counts),
+      std::end(counts),
+      0
     );
+    emp::vector<T> res(num_items);
 
+    // do gather, contributed items are only delivered to root process
     verify(MPI_Gatherv(
-      tokens.data(), // const void *sendbuf,
-      tokens.size(), // int sendcount,
-      mpi_type, // MPI_Datatype sendtype,
-      res.data(), // void *recvbuf,
-      counts.data(), // const int *recvcounts,
-      displacements.data(), // const int *displs,
-      mpi_type, // MPI_Datatype recvtype,
-      root, // int root,
+      items.data(), // const void *sendbuf
+      items.size(), // int sendcount
+      mpi_type, // MPI_Datatype sendtype
+      res.data(), // void *recvbuf
+      counts.data(), // const int *recvcounts
+      displacements.data(), // const int *displs
+      mpi_type, // MPI_Datatype recvtype
+      root, // int root
       comm // MPI_Comm comm
     ));
 
+    // if executing process is root, return gathered items
     return root == get_rank(comm)
       ? std::optional{res}
       : std::nullopt;
