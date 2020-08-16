ec751dd84ae5c4808ff3c73978663eb74d091ead
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-16 12:51:34 -0400

    Implement and stub test DistributedTimeoutBarrier

---
+++ tests/distributed/DistributedTimeoutBarrier.cpp
@@ -0,0 +1,18 @@
+#include <mpi.h>
+
+#define CATCH_CONFIG_DEFAULT_REPORTER "multiprocess"
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "distributed/DistributedTimeoutBarrier.hpp"
+#include "distributed/MPIGuard.hpp"
+#include "distributed/MultiprocessReporter.hpp"
+
+const uit::MPIGuard guard;
+
+TEST_CASE("Test DistributedTimeoutBarrier") {
+
+  // TODO flesh out stub test
+  uit::DistributedTimeoutBarrier{};
+
+}
