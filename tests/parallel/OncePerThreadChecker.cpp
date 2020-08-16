25e21cbf7c0910797329a37f2fe73ca7ca91f0b1
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-15 22:13:11 -0400

    Implement, stub test OncePerThreadChecker

---
+++ tests/parallel/OncePerThreadChecker.cpp
@@ -0,0 +1,12 @@
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "parallel/OncePerThreadChecker.hpp"
+
+TEST_CASE("OncePerThreadChecker") {
+
+  // TODO expand stub test
+  uit::OncePerThreadChecker checker;
+  checker.Check();
+
+}
