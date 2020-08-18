0019d6e64d16b919f8b5c30047d0f50b0bcdb861
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-18 10:51:48 -0400

    Exclude built docs from TODO grep

--- ci/grep_dotos.sh
+++ ci/grep_dotos.sh
@@ -2,4 +2,4 @@
 
 DIRECTORY_EXCLUDES=$(for submodule in $(ls -d third-party/*/); do printf " --exclude-dir=`basename $submodule`"; done)
 
-grep -iIER $DIRECTORY_EXCLUDES --exclude="*dotos*" 'todo|fixme' . | wc -l
+grep -iIER $DIRECTORY_EXCLUDES --exclude-dir=_build --exclude-dir=api --exclude-dir=doxyoutput --exclude="*dotos*" 'todo|fixme' . | wc -l
