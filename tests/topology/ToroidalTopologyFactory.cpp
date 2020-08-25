48c898f6b22c0913ce85ef87dbc94f2bdf2f8e12
rodsan0 <rodsan0@users.noreply.github.com>
2020-08-25 15:34:39 -0400

    Add test for ToroidalTopology

---
+++ tests/topology/ToroidalTopologyFactory.cpp
@@ -0,0 +1,57 @@
+#include <fstream>
+
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "topology/ToroidalTopologyFactory.hpp"
+/*
+TEST_CASE("Test ToroidalTopologyFactory") {
+
+  // N-dimensional case
+
+  for (size_t i = 0; i < 150; ++i) {
+    REQUIRE( uit::ToroidalTopologyFactory{}({i, i}).GetSize() == i * i );
+  }
+
+  for (const auto& node : uit::ToroidalTopologyFactory{}({10, 10})) {
+    REQUIRE( node.GetNumInputs() == 4);
+    REQUIRE( node.GetNumOutputs() == 4);
+  }
+}
+*/
+TEST_CASE("Test file output") {
+
+  // TODO flesh out stub test
+  uit::ToroidalTopologyFactory{}({3, 3}).PrintNodes();
+  std::cout << std::endl;
+  //uit::ToroidalTopologyFactory{}({5}).PrintEdges();
+
+
+  std::ofstream file1;
+  file1.open("1-torus-color.adj");
+  auto topo2 = uit::ToroidalTopologyFactory{}({100});
+  auto res2 = topo2.Optimize(5);
+  topo2.PrintPartition(res2, file1);
+  file1.close();
+
+  std::ofstream file2;
+  file2.open("2-torus-color.adj");
+
+  auto topo = uit::ToroidalTopologyFactory{}({10, 10});
+
+  topo.PrintNodes();
+  topo.AsCSR();
+
+  auto res = topo.Optimize(5);
+
+  topo.PrintPartition(res, file2);
+  file2.close();
+
+
+
+  std::ofstream file3;
+  file3.open("3-torus.adj");
+  uit::ToroidalTopologyFactory{}({4, 4, 4}).PrintNodes(file3);
+  file3.close();
+
+}
