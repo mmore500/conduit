8c0bbfe29b9bd5000d53d9a5d1b9292577aeb31c
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-29 22:11:50 -0400

    Update reading list

--- microbenchmarks/conduit/thread/HeadTailDuct.cpp
+++
@@ -1,9 +0,0 @@
-#include "conduit/ImplSpec.hpp"
-#include "conduit/thread/HeadTailDuct.hpp"
-
-using ImplSel = uit::ImplSelect<
-  uit::SerialPendingDuct,
-  uit::HeadTailDuct
->;
-
-#include "ThreadDuct.hpp"
