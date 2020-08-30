aad7d728b7377414ce21d2500978ad16843afeb8
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-08-30 12:53:59 -0400

    Add ToString member function to AlignedInherit

--- include/uit/parallel/AlignedInherit.hpp
+++ include/uit/parallel/AlignedInherit.hpp
@@ -8,6 +8,14 @@ namespace uit {
 template <typename T>
 class alignas(CACHE_LINE_SIZE) AlignedInherit : public T {
 
+public:
+
+  std::string ToString() const {
+    std::stringstream ss;
+    ss << *this;
+    return ss.str();
+  }
+
 };
 
 } // namespace uit
