616eb9561b5d787f226b0fb5f5a2f6ac41591f11
rodsan0 <rodsan0@users.noreply.github.com>
2020-08-31 16:14:12 -0400

    Add EnumeratedFunctor
    This functor is created from either an unordered_map or vector, and
    returns the value in it when operator() is called with an input

---
+++ include/utility/EnumeratedFunctor.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <unordered_map>
+
+#include "../third-party/Empirical/source/base/vector.h"
+
+namespace uit {
+
+template <typename Input, typename Output>
+class EnumeratedFunctor {
+    std::unordered_map<Input, Output> map;
+public:
+    EnumeratedFunctor(const std::unordered_map<Input, Output>& map_) : map(map_) { ; }
+    EnumeratedFunctor(const emp::vector<Output>& vect) {
+        for (size_t i = 0; i < vect.size(); ++i) map[i] = vect[i];
+    }
+
+    Output operator()(const Input& a) const { return map.at(a); }
+    size_t GetSize() const { return map.size(); }
+};
+
+}
