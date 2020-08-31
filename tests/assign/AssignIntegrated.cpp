81cfd57fb4a19db92fd48edb00b1785175f7a315
rodsan0 <rodsan0@users.noreply.github.com>
2020-08-31 15:57:54 -0400

    Added stub tests for assignments

---
+++ tests/assign/AssignIntegrated.cpp
@@ -0,0 +1,13 @@
+#include <mpi.h>
+
+#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_DEFAULT_REPORTER "multiprocess"
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "assign/AssignIntegrated.hpp"
+#include "distributed/MultiprocessReporter.hpp"
+#include "distributed/MPIGuard.hpp"
+
+const uit::MPIGuard guard;
+
+// stub test
