4e38b27f7522ee279532e48becf74bc07d88c31d
rodsan0 <rodsan0@users.noreply.github.com>
2020-08-31 16:19:10 -0400

    Add todo

--- include/utility/identity.hpp
+++ include/utility/identity.hpp
@@ -6,6 +6,7 @@ namespace uit {
 
 // TODO cpp20
 // adpated from https://codereview.stackexchange.com/q/134627
+// todo: move to polyfill
 static constexpr struct identity_t {
   template<typename T>
   constexpr decltype(auto) operator()(T&& t) const noexcept {
