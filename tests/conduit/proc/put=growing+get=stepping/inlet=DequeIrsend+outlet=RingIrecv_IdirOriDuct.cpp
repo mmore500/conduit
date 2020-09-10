d9e3b1287e448c13a857611f4b5bf8908d3db24a
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-09 22:46:21 -0400

    Fix path to python script in copied Makefile

--- tests/conduit/proc/put=growing+get=stepping/inlet=DequeIrsend+outlet=RingIrecv_IdirOriDuct.cpp
+++ tests/conduit/proc/put=growing+get=stepping/inlet=DequeIrsend+outlet=RingIrecv_IdirOriDuct.cpp
@@ -1,6 +1,6 @@
 #include "uit/conduit/ImplSpec.hpp"
 #include "uit/conduit/mock/ThrowDuct.hpp"
-#include "uit/conduit/proc/put=growing+get=stepping/inlet=DequeIrsend_outlet=RingIrecv_IdirOriDuct.hpp"
+#include "uit/conduit/proc/put=growing+get=stepping/inlet=DequeIrsend+outlet=RingIrecv_IdirOriDuct.hpp"
 
 using ImplSel = uit::ImplSelect<
   uit::SerialPendingDuct,
