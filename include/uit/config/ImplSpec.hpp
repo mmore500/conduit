b87c5450074aa8958ee91ff35bafd033875dc436
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-17 15:26:56 -0400

    Break defaults into own header

--- include/uit/config/ImplSpec.hpp
+++ include/uit/config/ImplSpec.hpp
@@ -1,22 +1,8 @@
 #pragma once
 
-#include "../ducts/intra/put=dropping+get=stepping+type=any/a::SerialPendingDuct.hpp"
-#include "../ducts/proc/put=dropping+get=stepping+type=trivial/inlet=RingIsend+outlet=RingIrecv_t::IriOriDuct.hpp"
-#include "../ducts/thread/put=dropping+get=stepping+type=any/a::AtomicPendingDuct.hpp"
+#include "defaults.hpp"
 
 namespace uit {
-
-// TODO don't use macros
-constexpr static size_t DEFAULT_BUFFER = 64;
-
-template<typename Spec>
-using DefaultIntraDuct = uit::a::SerialPendingDuct<Spec>;
-
-template<typename Spec>
-using DefaultThreadDuct = uit::a::AtomicPendingDuct<Spec>;
-
-template<typename Spec>
-using DefaultProcDuct = uit::t::IriOriDuct<Spec>;
 
 /**
  * Specifies the `Duct` implementations to be used for intra-thread, inter-
