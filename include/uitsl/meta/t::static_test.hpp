5e15cca92b57f585c7cb41e22751f1dbee5c83b1
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-19 23:31:23 -0400

    Test for trivially copyable, not trivial

--- include/uitsl/meta/t::static_test.hpp
+++ include/uitsl/meta/t::static_test.hpp
@@ -2,13 +2,13 @@
 
 #include <type_traits>
 
-#define uitsl_t_message "T is not a trivial type"
+#define uitsl_t_message "T is not a trivially copyable type"
 
 namespace uitsl {
 namespace t { // trivial type
 
 template<typename T>
-class StaticTest : public std::is_trivial<T> {};
+class StaticTest : public std::is_trivially_copyable<T> {};
 
 template<typename T>
 constexpr bool static_test() { return StaticTest<T>::value; }
