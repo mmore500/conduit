fcc3ab85e7881bb612f8e85c6b4b6f2eeb132af3
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-21 17:23:25 -0400

    Implement and test message-based accumulating ducts

---
+++ include/uitsl/distributed/MsgAccumulatorPacket.hpp
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "../../../third-party/Empirical/source/base/array.h"
+
+#include "../utility/NamedArrayElement.hpp"
+
+namespace uitsl {
+
+template<typename T>
+class MsgAccumulatorPacket {
+
+  T data{};
+  size_t epoch{};
+
+public:
+
+  MsgAccumulatorPacket() = default;
+
+  MsgAccumulatorPacket(const T& data_, const size_t& epoch_) {
+    data = data_;
+    epoch = epoch_;
+  }
+
+  MsgAccumulatorPacket& operator+=(const MsgAccumulatorPacket& rhs) {
+    GetData() += rhs.GetData();
+    BumpEpoch(rhs.GetEpoch());
+    return *this;
+  }
+
+  // MsgAccumulatorPacket& operator=(const MsgAccumulatorPacket&) = default;
+  //
+  // MsgAccumulatorPacket& operator=(MsgAccumulatorPacket&&) = default;
+
+  T& GetData() { return data; }
+
+  const T& GetData() const { return data; }
+
+  size_t GetEpoch() const { return epoch; }
+
+  void BumpEpoch(const size_t amt=1) { epoch += amt; }
+
+};
+
+} // namespace uitsl
---
+++ tests/uit/ducts/proc/accumulating+type=fundamental/double/inlet=Isend+outlet=Irecv_f::IiOiDuct.cpp
@@ -0,0 +1,14 @@
+#include "uit/ducts/intra/accumulating+type=any/a::AccumulatingDuct.hpp"
+#include "uit/ducts/mock/ThrowDuct.hpp"
+#include "uit/ducts/proc/accumulating+type=fundamental/inlet=Isend+outlet=Irecv_f::IiOiDuct.hpp"
+#include "uit/setup/ImplSpec.hpp"
+
+using ImplSel = uit::ImplSelect<
+  uit::a::AccumulatingDuct,
+  uit::ThrowDuct,
+  uit::f::IiOiDuct
+>;
+
+using MSG_T = double;
+
+#include "../../AccumulatingProcDuct.hpp"
