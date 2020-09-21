3c240210fdbb0af4dfed8fe32a429867060a5a02
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-21 19:01:08 -0400

    Move msg accumulating duct from f to t namespace

--- tests/uit/ducts/proc/outlet/accumulating+type=fundamental/f::WithdrawingWindowDuct.cpp
+++ tests/uit/ducts/proc/outlet/accumulating+type=fundamental/f::WithdrawingWindowDuct.cpp
@@ -4,8 +4,8 @@
 #define CATCH_CONFIG_MAIN
 #include "Catch/single_include/catch2/catch.hpp"
 
-#include "uitsl/mpi/MpiGuard.hpp"
 #include "uitsl/debug/MultiprocessReporter.hpp"
+#include "uitsl/mpi/MpiGuard.hpp"
 
 #include "uit/ducts/proc/outlet/accumulating+type=fundamental/f::WithdrawingWindowDuct.hpp"
 #include "uit/setup/ImplSpec.hpp"
