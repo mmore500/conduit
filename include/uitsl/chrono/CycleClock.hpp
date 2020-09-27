cccac686dbd0e50fe597c69c5d56d297fbb485f4
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-26 21:04:34 -0400

    Implement and test clock utils

--- include/uitsl/chrono/CycleClock.hpp
+++ include/uitsl/chrono/CycleClock.hpp
@@ -5,30 +5,33 @@
 #include <ctime>
 #include <ratio>
 
+#include "../../../third-party/benchmark/src/cycleclock.h"
+
 #include "chrono_utils.hpp"
+#include "cycle_freq.hpp"
 
 // quick-bench profiling results
-// gcc: https://quick-bench.com/q/cuGAJ2gORfc2KnG43IYElknSBeE
-// clang: https://quick-bench.com/q/wq_AmWxlB370-9kWRvqYOW9s094
-// ~280x faster than std::chrono clocks
+// gcc: https://quick-bench.com/q/WcbqUWBCoNBJvCP4n8h3kYfZDXU
+// clang: https://quick-bench.com/q/ezQiwgeEWnKsYgcf3yZHJ1arjwQ
+// ~77x faster than std::chrono clocks
 
 namespace uitsl {
 
-struct CoarseClock {
+struct CycleClock {
 
   // An arithmetic type or a class emulating an arithmetic type.
   // The representation type of C1::duration.
-  using rep = std::time_t;
+  using rep = size_t;
 
 	// A specialization of std::ratio.
   // The tick period of the clock in seconds.
-  using period = std::ratio<1, 1>;
+  using period = std::nano;
 
   // The duration type of the clock.
   using duration = std::chrono::duration<rep, period>;
 
   // The std::chrono::time_point type of the clock.
-  using time_point = std::chrono::time_point<CoarseClock>;
+  using time_point = std::chrono::time_point<CycleClock>;
 
   // true if t1 <= t2 is always true and the time between clock ticks
   // is constant, otherwise false
@@ -37,7 +40,9 @@ struct CoarseClock {
   // Returns a time_point object representing the current point in time.
   static time_point now() {
     return time_point{
-      std::chrono::seconds{std::time(NULL)}
+      std::chrono::nanoseconds{ static_cast<size_t>( std::round(
+        benchmark::cycleclock::Now() / uitsl::get_cycle_freq()
+      ) ) }
     };
   }
 
