db5270c68483f74b7ca0eb8cc511e556daad3055
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-09-30 18:47:45 -0400

    Implement and test inflate

---
+++ tests/uitsl/utility/inflate.cpp
@@ -0,0 +1,53 @@
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "uitsl/debug/compare_files.hpp"
+#include "uitsl/utility/inflate.hpp"
+
+TEST_CASE("inflate empty.tar.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/empty.tar.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/empty.tar", temp_path ) );
+
+}
+
+TEST_CASE("inflate jabberwocky.tar.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/jabberwocky.tar.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/jabberwocky.tar", temp_path ) );
+
+}
+
+TEST_CASE("inflate nested.tar.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/nested.tar.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/nested.tar", temp_path ) );
+
+}
+
+TEST_CASE("inflate rick.tar.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/rick.tar.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/rick.tar", temp_path ) );
+
+}
+
+TEST_CASE("inflate twas-brillig-and-the-slithy-toves-did-gyre-and-gimble-in-the-wabe-all-mimsy-were-the-borogoves-and-the-mome-raths-outgrabe-beware-the-jabberwock-my-son-the-jaws-that-bite-the-claws-that-catch-beware-the-jubjub-bird-and-shun-the-frumious-bandersnatch.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/twas-brillig-and-the-slithy-toves-did-gyre-and-gimble-in-the-wabe-all-mimsy-were-the-borogoves-and-the-mome-raths-outgrabe-beware-the-jabberwock-my-son-the-jaws-that-bite-the-claws-that-catch-beware-the-jubjub-bird-and-shun-the-frumious-bandersnatch.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/twas-brillig-and-the-slithy-toves-did-gyre-and-gimble-in-the-wabe-all-mimsy-were-the-borogoves-and-the-mome-raths-outgrabe-beware-the-jabberwock-my-son-the-jaws-that-bite-the-claws-that-catch-beware-the-jubjub-bird-and-shun-the-frumious-bandersnatch", temp_path ) );
+
+}
+
+TEST_CASE("inflate zen.tar.gz") {
+
+  const auto temp_path{ uitsl::inflate( "assets/zen.tar.gz" ) };
+
+  REQUIRE( uitsl::compare_files( "assets/zen.tar", temp_path ) );
+
+}
