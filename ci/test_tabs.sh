e9c093027441f3016d75e08f939c691e6ae7c9d7
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-09-30 20:41:11 -0400

    Update test_tabs.sh

--- ci/test_tabs.sh
+++ ci/test_tabs.sh
@@ -8,5 +8,6 @@ if [ "${SOURCE_HASH}" == "$( find -path ./third-party -prune -false -o -type f |
 then
   exit 0 # success
 else
+  echo "tab characters detected, run ./ci/replace_tabs.sh locally to find & fix"
   exit 1 # failure
 fi
