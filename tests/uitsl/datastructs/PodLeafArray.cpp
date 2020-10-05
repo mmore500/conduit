c65502003dc1c30f06ee6a5b8e7c60205b25697e
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-05 03:57:59 -0400

    Implement and test Pod system

---
+++ tests/uitsl/datastructs/PodLeafArray.cpp
@@ -0,0 +1,11 @@
+#define CATCH_CONFIG_MAIN
+#include "Catch/single_include/catch2/catch.hpp"
+
+#include "uitsl/datastructs/PodLeafArray.hpp"
+
+TEST_CASE("Test PodLeafArray") {
+
+  // TODO expand stub test
+  uitsl::PodLeafArray<char, 2>{};
+
+}
