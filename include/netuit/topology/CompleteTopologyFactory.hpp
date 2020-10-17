05fff55979e87152b6e2a685e44b60d7ad7781b7
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-17 18:44:55 -0400

    Implement and test complete topology factory

---
+++ include/netuit/topology/CompleteTopologyFactory.hpp
@@ -0,0 +1,51 @@
+#pragma once
+#ifndef NETUIT_TOPOLOGY_DYADICTOPOLOGYFACTORY_HPP_INCLUDE
+#define NETUIT_TOPOLOGY_DYADICTOPOLOGYFACTORY_HPP_INCLUDE
+
+#include "../../../third-party/Empirical/source/base/vector.h"
+
+#include "TopoEdge.hpp"
+#include "Topology.hpp"
+#include "TopoNode.hpp"
+
+namespace netuit {
+
+netuit::Topology make_complete_topology(const size_t cardinality) {
+
+  emp::vector<netuit::TopoNode> res( cardinality );
+
+  size_t edge_counter{};
+
+  for (size_t i{}; i < cardinality; ++i) {
+    for (size_t j{}; j < cardinality; ++j) {
+      if ( i != j ) { // exclude self-loops
+        netuit::TopoEdge edge{edge_counter++};
+        res[i].AddInput( edge.GetOutlet() );
+        res[j].AddOutput( edge.GetInlet() );
+      }
+    }
+  }
+
+  return netuit::Topology{ res };
+
+}
+
+struct CompleteTopologyFactory {
+
+  netuit::Topology operator()(const size_t cardinality) const {
+    return make_complete_topology(cardinality);
+  }
+  netuit::Topology operator()(const emp::vector<size_t> cardinality) const {
+    emp_assert(cardinality.size() == 1);
+    return make_complete_topology(cardinality.front());
+  }
+  static std::string GetName() { return "Complete Topology"; }
+
+  // todo static?
+  std::string GetSlug() const { return "complete"; }
+
+};
+
+} // namespace netuit
+
+#endif // #ifndef NETUIT_TOPOLOGY_DYADICTOPOLOGYFACTORY_HPP_INCLUDE
