1dc2a7da95e4134f4d60810bc3f82c7cad36f29d
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-18 23:28:25 -0400

    Fixup audit_compare

--- include/uitsl/debug/audit_compare.hpp
+++ include/uitsl/debug/audit_compare.hpp
@@ -2,68 +2,76 @@
 #ifndef UITSL_DEBUG_AUDIT_COMPARE_HPP_INCLUDE
 #define UITSL_DEBUG_AUDIT_COMPARE_HPP_INCLUDE
 
-#include "safe_compare.hpp"
+#include <limits>
+#include <type_traits>
+
+#include "audit_cast.hpp"
 
 namespace uitsl {
 
 template <typename I, typename J>
 inline bool audit_greater(const I x, const J y) {
-  #ifndef NDEBUG
-    return uitsl::safe_greater( x , y );
-  #else
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wsign-compare"
-    return x > y;
-    #pragma GCC diagnostic pop
-  #endif
+
+  if constexpr (
+    std::numeric_limits<I>::is_signed != std::numeric_limits<J>::is_signed
+  ) {
+    return uitsl::audit_cast<typename std::make_signed<I>::type>( x )
+      > uitsl::audit_cast<typename std::make_signed<J>::type>( y )
+    ;
+  } else return x > y;
+
 }
 
 template <typename I, typename J>
 inline bool audit_less(const I x, const J y) {
-  #ifndef NDEBUG
-    return uitsl::safe_less( x , y );
-  #else
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wsign-compare"
-    return x < y;
-    #pragma GCC diagnostic pop
-  #endif
+
+  if constexpr (
+    std::numeric_limits<I>::is_signed != std::numeric_limits<J>::is_signed
+  ) {
+    return uitsl::audit_cast<typename std::make_signed<I>::type>( x )
+      < uitsl::audit_cast<typename std::make_signed<J>::type>( y )
+    ;
+  } else return x < y;
+
 }
 
 template <typename I, typename J>
 inline bool audit_leq(const I x, const J y) {
-  #ifndef NDEBUG
-    return uitsl::safe_leq( x , y );
-  #else
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wsign-compare"
-    return x <= y;
-    #pragma GCC diagnostic pop
-  #endif
+
+  if constexpr (
+    std::numeric_limits<I>::is_signed != std::numeric_limits<J>::is_signed
+  ) {
+    return uitsl::audit_cast<typename std::make_signed<I>::type>( x )
+      <= uitsl::audit_cast<typename std::make_signed<J>::type>( y )
+    ;
+  } else return x <= y;
+
 }
 
 template <typename I, typename J>
 inline bool audit_geq(const I x, const J y) {
-  #ifndef NDEBUG
-    return uitsl::safe_geq( x , y );
-  #else
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wsign-compare"
-    return x >= y;
-    #pragma GCC diagnostic pop
-  #endif
+
+  if constexpr (
+    std::numeric_limits<I>::is_signed != std::numeric_limits<J>::is_signed
+  ) {
+    return uitsl::audit_cast<typename std::make_signed<I>::type>( x )
+      >= uitsl::audit_cast<typename std::make_signed<J>::type>( y )
+    ;
+  } else return x >= y;
+
 }
 
 template <typename I, typename J>
 inline bool audit_equal(const I x, const J y) {
-  #ifndef NDEBUG
-    return uitsl::safe_equal( x , y );
-  #else
-    #pragma GCC diagnostic push
-    #pragma GCC diagnostic ignored "-Wsign-compare"
-    return x == y;
-    #pragma GCC diagnostic pop
-  #endif
+
+  if constexpr (
+    std::numeric_limits<I>::is_signed != std::numeric_limits<J>::is_signed
+  ) {
+    return uitsl::audit_cast<typename std::make_signed<I>::type>( x )
+      == uitsl::audit_cast<typename std::make_signed<J>::type>( y )
+    ;
+  } else return x == y;
+
 }
 
 } // namespace uitsl
