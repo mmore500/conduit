66a6c2186e2e2dd8f4e7f6e43e13c4bf52809235
Matthew Andres Moreno <mmore500.login@gmail.com>
2020-10-21 00:39:23 -0400

    Use double quotes to prevent shell shenanigans

--- ci/alphabetize_includes.sh
+++ ci/alphabetize_includes.sh
@@ -8,17 +8,17 @@ do
   printf "."
 
   # adapted from https://stackoverflow.com/a/6970681
-  include_linenos=$(awk '/^#include .*/ {print FNR}' $filename)
+  include_linenos=$(awk '/^#include .*/ {print FNR}' "$filename")
 
   # adapted from https://stackoverflow.com/a/26809816
   # and https://superuser.com/a/284192
-  echo $include_linenos | tr ' ' '\n' | awk 'NR==1{first=$1;last=$1;next} $1 == last+1 {last=$1;next} {print first,last;first=$1;last=first} END{print first,last}' | while read line ; do
+  echo "$include_linenos" | tr ' ' '\n' | awk 'NR==1{first=$1;last=$1;next} $1 == last+1 {last=$1;next} {print first,last;first=$1;last=first} END{print first,last}' | while read line ; do
 
     # adapted from https://unix.stackexchange.com/a/11064
-    read start stop <<< $line
+    read start stop <<< "$line"
 
     # adapted from https://stackoverflow.com/a/46018238
-    echo "x" | ex -s -c "${start},${stop}!sort -fd" $filename
+    echo "x" | ex -s -c "${start},${stop}!sort -fd" "$filename"
 
   done
 done
