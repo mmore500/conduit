963b62a267834bc6e7435162b3898f97d7a1fd94
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-20 22:51:03 -0400

    Fix whitespace

--- third-party/install_metis.sh
+++ third-party/install_metis.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-apt-get install -y libmetis-dev metis ||\
-pacman -S metis ||\
-yum install metis ||\
-echo "package installer not supported! please install METIS manually"
+apt-get install -y libmetis-dev metis \
+  || pacman -S metis \
+  || yum install metis \
+  || echo "package installer not supported! please install METIS manually"
