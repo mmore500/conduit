fa8c1e9b423f25a78b562a9a79dbc5609568938c
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-20 23:06:29 -0400

    Install benchmark via script, not subrepo

--- third-party/install_benchmark.sh
+++ third-party/install_benchmark.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-git clone https://github.com/google/googletest.git benchmark/googletest
+git clone --recursive https://github.com/google/benchmark
 cd benchmark
+git reset --hard 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
+git clone https://github.com/google/googletest
 mkdir build && cd build
 cmake ../
 make
