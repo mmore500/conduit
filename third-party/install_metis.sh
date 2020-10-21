1d5fda7bec5d675cdd1deb4c080a82a22d74eaf3
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-20 22:54:26 -0400

    Pin metis version

--- third-party/install_metis.sh
+++ third-party/install_metis.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-apt-get install -y libmetis-dev metis \
+apt-get install -y libmetis-dev=5.1.0.dfsg-5 metis=5.1.0.dfsg-5 \
+  || apt-get install libmetis-dev metis \
   || pacman -S metis \
   || yum install metis \
   || echo "package installer not supported! please install METIS manually"
