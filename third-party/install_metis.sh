1a91d825aad061651cb6ab6b0c06769f335905fa
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-10-22 09:58:41 -0400

    Apt-update and apt clean for metis install

--- third-party/install_metis.sh
+++ third-party/install_metis.sh
@@ -1,6 +1,11 @@
 #!/bin/sh
 
-apt-get install -y libmetis-dev=5.1.0.dfsg-5 metis=5.1.0.dfsg-5 \
+apt-get update -qq \
+  && apt-get install -qq --no-install-recommends \
+    libmetis-dev=5.1.0.dfsg-5 \
+    metis=5.1.0.dfsg-5 \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/* \
   || apt-get install libmetis-dev metis \
   || pacman -S metis \
   || yum install metis \
