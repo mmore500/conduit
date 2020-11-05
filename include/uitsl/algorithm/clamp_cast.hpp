ad632ef914246deca85d5f2cbae9f782f4361e64
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-11-05 15:30:57 -0500

    Fixup clamp_cast

--- include/uitsl/algorithm/clamp_cast.hpp
+++ include/uitsl/algorithm/clamp_cast.hpp
@@ -3,6 +3,7 @@
 #define UITSL_ALGORITHM_CLAMP_CAST_HPP_INCLUDE
 
 #include <algorithm>
+#include <cmath>
 #include <limits>
 
 #include "nan_to_zero.hpp"
@@ -11,10 +12,31 @@ namespace uitsl {
 
 template<typename To, typename From>
 To clamp_cast( const From from ) {
+
+
+  #ifndef __EMSCRIPTEN__
+  constexpr
+  #else
+  const
+  #endif
+  double lower_bound = static_cast<double>( std::nexttoward(
+    std::numeric_limits<To>::min(), std::numeric_limits<double>::infinity()
+  ) );
+
+
+  #ifndef __EMSCRIPTEN__
+  constexpr
+  #else
+  const
+  #endif
+  double upper_bound = static_cast<double>( std::nexttoward(
+    std::numeric_limits<To>::max(), -std::numeric_limits<double>::infinity()
+  ) );
+
   return static_cast<To>( std::clamp(
     static_cast<double>( uitsl::nan_to_zero( from ) ),
-    static_cast<double>( std::numeric_limits<To>::min() ),
-    static_cast<double>( std::numeric_limits<To>::max() )
+    lower_bound,
+    upper_bound
   ) );
 }
 
