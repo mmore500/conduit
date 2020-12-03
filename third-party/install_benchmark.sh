8d48b6a4d6080f33e02ef190da86fa593ff45636
rodsan0 <rodsan0@users.noreply.github.com>
2020-12-02 20:09:00 -0500

    Fix deps install

--- third-party/install_benchmark.sh
+++ third-party/install_benchmark.sh
@@ -1,11 +1,16 @@
 #!/bin/sh
 
+# Clone Google Benchmark
 git clone --recursive https://github.com/google/benchmark
+# Go to the library root directory
 cd benchmark
+# Use pinned commit
 git reset --hard 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
+# Clone Google Test (as a dependency)
 git clone https://github.com/google/googletest
-mkdir build && cd build
-cmake ../
-make
-make test
-make install
+# Make a build directory to place the build output.
+cmake -E make_directory "build"
+# Generate build system files with cmake.
+cmake -DCMAKE_BUILD_TYPE=Release -S . -B "build"
+# Build the library.
+cmake --build "build" --config Release
--- third-party/install_cmake.sh
+++ third-party/install_cmake.sh
@@ -8,4 +8,4 @@ wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh
 mkdir /opt/cmake
 yes | sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
 
-ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
+ln -s /opt/cmake/cmake-$version.$build-Linux-x86_64/bin/cmake /usr/local/bin/cmake
