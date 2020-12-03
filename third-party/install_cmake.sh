d331e2d75c4cabdddd40db05cad778b23bce15a5
rodsan0 <rodsan0@users.noreply.github.com>
2020-12-02 19:29:40 -0500

    Yes

--- third-party/install_cmake.sh
+++ third-party/install_cmake.sh
@@ -6,6 +6,6 @@ mkdir ~/temp
 cd ~/temp
 wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh
 mkdir /opt/cmake
-sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
+yes | sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
 
 ln -s /opt/cmake/bin/cmake /usr/local/bin/cmake
