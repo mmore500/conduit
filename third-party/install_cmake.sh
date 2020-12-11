a14df3beb79582a3382d2875424f21e4933c1226
rodsan0 <rodsan0@users.noreply.github.com>
2020-12-11 18:00:19 -0500

    Add ctest to path in Dockerfile

--- third-party/install_cmake.sh
+++ third-party/install_cmake.sh
@@ -8,5 +8,6 @@ wget https://cmake.org/files/v$version/cmake-$version.$build-Linux-x86_64.sh
 mkdir /opt/cmake
 yes | sh cmake-$version.$build-Linux-x86_64.sh --prefix=/opt/cmake
 
-rm -f /usr/local/bin/cmake
+rm -f /usr/local/bin/cmake /usr/local/bin/ctest
 ln -s /opt/cmake/cmake-$version.$build-Linux-x86_64/bin/cmake /usr/local/bin/cmake
+ln -s /opt/cmake/cmake-$version.$build-Linux-x86_64/bin/ctest /usr/local/bin/ctest
