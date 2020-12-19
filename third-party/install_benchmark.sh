21fb23e781bc0fdce66aafa399ea7db181127357
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-19 14:18:48 -0500

    Get google benchmark from package manager

--- third-party/install_benchmark.sh
+++ third-party/install_benchmark.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/google/benchmark
-cd benchmark
-git reset --hard 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
-git clone https://github.com/google/googletest
-mkdir build && cd build
-cmake ../
-make
-make test
-make install
+#!/bin/sh
+
+apt-get update -qq \
+  && apt-get install -qq --no-install-recommends \
+    libbenchmark1 \
+    libbenchmark-dev \
+  && apt-get clean \
+  && rm -rf /var/lib/apt/lists/* \
+  || echo "package installer not supported!"
+    && echo "please install Google Benchmark manually"
