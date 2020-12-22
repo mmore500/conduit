6db55a5bafd5d2d30d115adfdb868199372360df
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2020-12-22 17:35:45 -0500

    Install benchmark via repos AND clone subrepo

--- third-party/install_benchmark.sh
+++ third-party/install_benchmark.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
+git clone --recursive https://github.com/google/benchmark
+cd benchmark
+git reset --hard 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
+git clone https://github.com/google/googletest
+
 apt-get update -qq \
   && apt-get install -qq --no-install-recommends \
     libbenchmark1 \
