97a2e55c68b7b31a0bbd4979b4b68da2ef0f0efe
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2020-12-23 16:32:32 -0500

    Add quiet flag only to OpenMPI

--- cmake/NProcMultiplexer.cmake
+++ cmake/NProcMultiplexer.cmake
@@ -2,7 +2,12 @@ include("../cmake/CatchInt.cmake")
 
 function(nproc_multiplex_tests TARGET)
     foreach(nproc RANGE 1 8)
-        set(OptionalCatchTestLauncher ${MPIEXEC} --quiet ${MPIEXEC_NUMPROC_FLAG} ${nproc})
+        if (${MPI_EXECUTABLE_SUFFIX} STREQUAL ".openmpi")
+            set(QuietFlag "--quiet")
+        else()
+            set(QuietFlag "")
+        endif()
+        set(OptionalCatchTestLauncher ${MPIEXEC} ${QuietFlag} ${MPIEXEC_NUMPROC_FLAG} ${nproc})
         catch_discover_tests(${TARGET}
             TEST_SPEC "[nproc:${nproc}]"
             TEST_SUFFIX "-nproc${nproc}"
