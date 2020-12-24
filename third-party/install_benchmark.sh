8d99d92151dbb3b5e4df606eb1a47b3f6a88e2e8
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-12-24 11:03:27 -0600

    Install benchmark from source
    We need access to features not available in the package manager version

--- third-party/install_benchmark.sh
+++ third-party/install_benchmark.sh
@@ -1,15 +1,30 @@
 #!/bin/sh
 
-git clone --recursive https://github.com/google/benchmark
-cd benchmark
-git reset --hard 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
-git clone https://github.com/google/googletest
+# adapted from https://stackoverflow.com/a/43136160
+mkdir benchmark && cd benchmark
+git init
+git remote add origin https://github.com/google/benchmark
+git fetch --depth 1 origin 37177a84b7e8d33696ea1e1854513cb0de3b4dc3
+git checkout FETCH_HEAD
 
-apt-get update -qq \
-  && apt-get install -qq --no-install-recommends \
-    libbenchmark1 \
-    libbenchmark-dev \
-  && apt-get clean \
-  && rm -rf /var/lib/apt/lists/* \
-  || echo "package installer not supported!"
-    && echo "please install Google Benchmark manually"
+# adapted from https://stackoverflow.com/a/43136160
+mkdir googletest && cd googletest
+git init
+git remote add origin https://github.com/google/googletest
+git fetch --depth 1 origin ca4b7c9ff4d8a5c37ac51795b03ffe934958aeff
+git checkout FETCH_HEAD
+cd ..
+
+cmake -E make_directory "build"
+cmake -E chdir "build" cmake -DCMAKE_BUILD_TYPE=Release ../
+cmake --build "build" --config Release --target install
+
+# pacakge manger install isn't new enough, disable it
+# apt-get update -qq \
+#   && apt-get install -qq --no-install-recommends \
+#     libbenchmark1 \
+#     libbenchmark-dev \
+#   && apt-get clean \
+#   && rm -rf /var/lib/apt/lists/* \
+#   || echo "package installer not supported!"
+#     && echo "please install Google Benchmark manually"
