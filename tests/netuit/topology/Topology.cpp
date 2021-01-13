e6d1212868e4043e31e077ddee32bece27ec4574
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-01-12 22:10:44 -0500

    Reorder neighbors in topology tests

--- tests/netuit/topology/Topology.cpp
+++ tests/netuit/topology/Topology.cpp
@@ -70,15 +70,15 @@ TEST_CASE("Test AsCSR ToroidalTopologyFactory", "[nproc:1]") {
 
   REQUIRE( x_adj == emp::vector<int>{ 0, 4, 8, 12, 16, 20, 24, 28, 32, 36 } );
   REQUIRE( adjacency == emp::vector<int>{
-    1, 2, 3, 6, // 0's neighbors
-    2, 0, 4, 7, // 1's neighbors
-    0, 1, 5, 8, // 2's neighbors
-    4, 5, 6, 0, // 3's neighbors
-    5, 3, 7, 1, // 4's neighbors
-    3, 4, 8, 2, // 5's neighbors
-    7, 8, 0, 3, // 6's neighbors
-    8, 6, 1, 4, // 7's neighbors
-    6, 7, 2, 5, // 8's neighbors
+    6, 2, 1, 3, // 0's neighbors
+    7, 0, 2, 4, // 1's neighbors
+    8, 1, 0, 5, // 2's neighbors
+    0, 5, 4, 6, // 3's neighbors
+    1, 3, 5, 7, // 4's neighbors
+    2, 4, 3, 8, // 5's neighbors
+    3, 8, 7, 0, // 6's neighbors
+    4, 6, 8, 1, // 7's neighbors
+    5, 7, 6, 2, // 8's neighbors
   } );
 
 
@@ -101,15 +101,15 @@ TEST_CASE("Test trivial Subtopology, ToroidalTopologyFactory", "[nproc:1]") {
 
   REQUIRE( x_adj == emp::vector<int>{ 0, 4, 8, 12, 16, 20, 24, 28, 32, 36 } );
   REQUIRE( adjacency == emp::vector<int>{
-    2, 1, 6, 3, // 0's neighbors
-    0, 2, 7, 4, // 1's neighbors
-    1, 0, 8, 5, // 2's neighbors
-    5, 4, 0, 6, // 3's neighbors
-    3, 5, 1, 7, // 4's neighbors
-    4, 3, 2, 8, // 5's neighbors
-    8, 7, 3, 0, // 6's neighbors
-    6, 8, 4, 1, // 7's neighbors
-    7, 6, 5, 2, // 8's neighbors
+    3, 1, 2, 6, // 0's neighbors
+    4, 2, 0, 7, // 1's neighbors
+    5, 0, 1, 8, // 2's neighbors
+    6, 4, 5, 0, // 3's neighbors
+    7, 5, 3, 1, // 4's neighbors
+    8, 3, 4, 2, // 5's neighbors
+    0, 7, 8, 3, // 6's neighbors
+    1, 8, 6, 4, // 7's neighbors
+    2, 6, 7, 5, // 8's neighbors
   } );
 
 }
@@ -123,12 +123,12 @@ TEST_CASE("Test Subtopology, ToroidalTopologyFactory", "[nproc:1]") {
 
   REQUIRE( x_adj == emp::vector<int>{ 0, 3, 6, 9, 12, 15, 18 } );
   REQUIRE( adjacency == emp::vector<int>{
-    2, 1, 3, // 0's neighbors
-    0, 2, 4, // 1's neighbors
-    1, 0, 5, // 2's neighbors
-    5, 4, 0, // 3's neighbors
-    3, 5, 1, // 4's neighbors
-    4, 3, 2, // 5's neighbors
+    3, 1, 2, // 0's neighbors
+    4, 2, 0, // 1's neighbors
+    5, 0, 1, // 2's neighbors
+    4, 5, 0, // 3's neighbors
+    5, 3, 1, // 4's neighbors
+    3, 4, 2, // 5's neighbors
   } );
 
 
