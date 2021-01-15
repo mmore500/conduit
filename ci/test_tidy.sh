fe130fe22a38d51438e8976be54340a92b6a5d10
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-14 21:05:34 -0500

    Add include alphabetization to test_tidy.sh

--- ci/test_tidy.sh
+++ ci/test_tidy.sh
@@ -2,6 +2,7 @@
 
 set -e
 
+./ci/test_alphabetize_include.sh && echo "✔ include alphabetization ok" || exit 1
 ./ci/test_headerguards.sh && echo "✔ headerguards ok" || exit 1
 ./ci/test_merge_conflict_markers.sh && echo "✔ conflict markers ok" || exit 1
 ./ci/test_missing_newlines.sh && echo "✔ EOF newlines ok" || exit 1
