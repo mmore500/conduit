cc4151b85275076e60ae21bd3e00a3a4f8b1c254
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-24 21:26:11 -0500

    Bugfix: break by thread id of sender AND receiver

--- include/uit/ducts/proc/impl/backend/PooledBackEnd.hpp
+++ include/uit/ducts/proc/impl/backend/PooledBackEnd.hpp
@@ -2,7 +2,11 @@
 #ifndef UIT_DUCTS_PROC_IMPL_BACKEND_POOLEDBACKEND_HPP_INCLUDE
 #define UIT_DUCTS_PROC_IMPL_BACKEND_POOLEDBACKEND_HPP_INCLUDE
 
+#include <tuple>
+
 #include "../../../../../../third-party/Empirical/include/emp/base/vector.hpp"
+#include "../../../../../../third-party/Empirical/include/emp/datastructs/tuple_utils.hpp"
+#include "../../../../../../third-party/Empirical/third-party/robin-hood-hashing/src/include/robin_hood.h"
 
 #include "../../../../../uitsl/datastructs/VectorMap.hpp"
 
@@ -39,87 +43,61 @@ public:
 
 private:
 
-  // thread_id of caller -> proc_id of target -> inlet pool
-  uitsl::VectorMap<
-    uitsl::thread_id_t,
-    uitsl::VectorMap<
-      uitsl::proc_id_t,
-      inlet_pool_t
-    >
+  // < caller thread, target thread, target proc > -> inlet pool
+  robin_hood::unordered_map<
+    std::tuple<uitsl::thread_id_t, uitsl::thread_id_t, uitsl::proc_id_t>,
+    inlet_pool_t,
+    emp::TupleHash<uitsl::thread_id_t, uitsl::thread_id_t, uitsl::proc_id_t>
   > inlet_pools;
 
-  // thread_id of caller -> proc_id of target -> outlet pool
-  uitsl::VectorMap<
-    uitsl::thread_id_t,
-    uitsl::VectorMap<
-      uitsl::proc_id_t,
-      outlet_pool_t
-    >
+  // < caller thread, target thread, target proc > -> inlet pool
+  robin_hood::unordered_map<
+    std::tuple<uitsl::thread_id_t, uitsl::thread_id_t, uitsl::proc_id_t>,
+    outlet_pool_t,
+    emp::TupleHash<uitsl::thread_id_t, uitsl::thread_id_t, uitsl::proc_id_t>
   > outlet_pools;
 
   bool AreAllInletPoolsInitialized() const {
 
     // check that all windows are in the same initialization state
-    emp_assert( std::all_of(
+    emp_assert( std::adjacent_find(
       std::begin(inlet_pools), std::end(inlet_pools),
-      [this](const auto& map_pair){
-        const auto& [key, map] = map_pair;
-        return std::all_of(
-          std::begin(map), std::end(map),
-          [this](const auto& pool_pair) {
-            const auto& [key, pool] = pool_pair;
-            const auto& rep = inlet_pools.begin()->second.begin()->second;
-            return pool.IsInitialized() == rep.IsInitialized();
-          }
-        );
+      [this](const auto& pool_pair1, const auto& pool_pair2) {
+        const auto& [key1, pool1] = pool_pair1;
+        const auto& [key2, pool2] = pool_pair2;
+        return pool1.IsInitialized() != pool2.IsInitialized();
       }
-    ) );
+    ) == std::end(inlet_pools) );
 
 
     return std::any_of(
       std::begin(inlet_pools), std::end(inlet_pools),
-      [](const auto& map_pair){
-        const auto& [key, map] = map_pair;
-        return std::any_of(
-          std::begin(map), std::end(map),
-          [](const auto& pool_pair) {
-            const auto& [key, pool] = pool_pair;
-            return pool.IsInitialized();
-          }
-        );
+      [](const auto& pool_pair) {
+        const auto& [key, pool] = pool_pair;
+        return pool.IsInitialized();
       }
     );
+
   }
 
   bool AreAllOutletPoolsInitialized() const {
 
     // check that all windows are in the same initialization state
-    emp_assert( std::all_of(
+    emp_assert( std::adjacent_find(
       std::begin(outlet_pools), std::end(outlet_pools),
-      [this](const auto& map_pair){
-        const auto& [key, map] = map_pair;
-        return std::all_of(
-          std::begin(map), std::end(map),
-          [this](const auto& pool_pair) {
-            const auto& [key, pool] = pool_pair;
-            const auto& rep = outlet_pools.begin()->second.begin()->second;
-            return pool.IsInitialized() == rep.IsInitialized()  ;
-          }
-        );
+      [this](const auto& pool_pair1, const auto& pool_pair2) {
+        const auto& [key1, pool1] = pool_pair1;
+        const auto& [key2, pool2] = pool_pair2;
+        return pool1.IsInitialized() != pool2.IsInitialized();
       }
-    ) );
+    ) == std::end(outlet_pools) );
+
 
     return std::any_of(
       std::begin(outlet_pools), std::end(outlet_pools),
-      [](const auto& map_pair){
-        const auto& [key, map] = map_pair;
-        return std::any_of(
-          std::begin(map), std::end(map),
-          [](const auto& pool_pair) {
-            const auto& [key, pool] = pool_pair;
-            return pool.IsInitialized();
-          }
-        );
+      [](const auto& pool_pair) {
+        const auto& [key, pool] = pool_pair;
+        return pool.IsInitialized();
       }
     );
   }
@@ -141,35 +119,27 @@ public:
 
   void RegisterInletSlot(const address_t& address) {
     emp_assert( !IsInitialized() );
-    inlet_pools[
-      address.GetInletThread()
-    ][
-      address.GetOutletProc()
-    ].Register(address);
+    inlet_pools[ {
+        address.GetInletThread(),
+        address.GetOutletThread(),
+        address.GetOutletProc()
+    } ].Register(address);
   }
 
   void RegisterOutletSlot(const address_t& address) {
     emp_assert( !IsInitialized() );
-    outlet_pools[
-      address.GetOutletThread()
-    ][
-      address.GetInletProc()
-    ].Register(address);
+    outlet_pools[ {
+        address.GetInletThread(),
+        address.GetOutletThread(),
+        address.GetOutletProc()
+    } ].Register(address);
   }
 
   void Initialize() {
     emp_assert( !IsInitialized() );
 
-    for (auto& [__, proc_map] : inlet_pools) {
-      for (auto& [__, pool] : proc_map) {
-        pool.Initialize();
-      }
-    }
-    for (auto& [__, proc_map] : outlet_pools) {
-      for (auto& [__, pool] : proc_map) {
-        pool.Initialize();
-      }
-    }
+    for (auto& [__, pool] : inlet_pools) pool.Initialize();
+    for (auto& [__, pool] : outlet_pools) pool.Initialize();
 
     emp_assert( IsInitialized() || IsEmpty() );
   }
@@ -177,11 +147,11 @@ public:
   inlet_pool_t& GetInletPool(const address_t& address) {
     emp_assert( IsInitialized() );
 
-    auto& pool = inlet_pools.at(
-      address.GetInletThread()
-    ).at(
-      address.GetOutletProc()
-    );
+    auto& pool = inlet_pools.at( {
+      address.GetInletThread(),
+      address.GetOutletThread(),
+      address.GetOutletProc(),
+    } );
 
     emp_assert( pool.IsInitialized(), pool.GetSize() );
 
@@ -191,11 +161,11 @@ public:
   outlet_pool_t& GetOutletPool(const address_t& address) {
     emp_assert( IsInitialized() );
 
-    auto& pool = outlet_pools.at(
-      address.GetOutletThread()
-    ).at(
-      address.GetInletProc()
-    );
+    auto& pool = outlet_pools.at( {
+      address.GetInletThread(),
+      address.GetOutletThread(),
+      address.GetOutletProc(),
+    } );
 
     emp_assert( pool.IsInitialized() );
 
