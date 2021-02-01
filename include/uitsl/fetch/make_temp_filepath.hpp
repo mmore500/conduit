316622e217a24c8501535684cc8b959420141a23
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2021-01-31 19:28:16 -0500

    Fix file handle link in make_temp_filepath

--- include/uitsl/fetch/make_temp_filepath.hpp
+++ include/uitsl/fetch/make_temp_filepath.hpp
@@ -2,20 +2,16 @@
 #ifndef UITSL_FETCH_MAKE_TEMP_FILEPATH_HPP_INCLUDE
 #define UITSL_FETCH_MAKE_TEMP_FILEPATH_HPP_INCLUDE
 
-#include <cstdlib>
-#include <string>
-
-#include "../debug/err_audit.hpp"
 #include "../polyfill/filesystem.hpp"
+
+#include "make_temp_dirpath.hpp"
 
 namespace uitsl {
 
 inline std::filesystem::path make_temp_filepath() {
 
-  // adapted from https://en.wikipedia.org/wiki/Mkstemp
-  char dest_path[] = "/tmp/uitsl_temp_fileXXXXXX";
-  uitsl_err_audit(! mkstemp(dest_path) );
-  return dest_path;
+  // don't use mkstmp because that leaks file handles
+  return uitsl::make_temp_dirpath() / "uitsl_temp_file";
 
 }
 
