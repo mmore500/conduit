2e7705de0ea195a069a01fab12f44be2cd871f7d
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-06-23 14:33:16 -0400

    Exclude binary files from whitespace fixup

--- ci/replace_tabs.sh
+++ ci/replace_tabs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*Makefile" ! -path "*Maketemplate*" ! -path "./node_modules/*")
+# greq -qI tests if file is binary
+# adapted from https://stackoverflow.com/a/29689345
+TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*Makefile" ! -path "*Maketemplate*" ! -path "./node_modules/*" -exec grep -qI . {} ';' -print)
 
 for filename in $TARGETS
 do
