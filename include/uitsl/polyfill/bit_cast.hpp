07ccf1169903ea9ac0f22c0269e7be904c402768
Santiago Rodriguez Papa <rodsan0@users.noreply.github.com>
2021-08-10 18:32:38 -0400

    Add bit_cast polyfill

---
+++ include/uitsl/polyfill/bit_cast.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#ifndef UITSL_POLYFILL_BIT_CAST_HPP_INCLUDE
+#define UITSL_POLYFILL_BIT_CAST_HPP_INCLUDE
+
+#if __cplusplus <= 201703L
+
+#include <type_traits>
+
+namespace std {
+
+    // Adapted from https://en.cppreference.com/w/cpp/numeric/bit_cast
+    template <class To, class From>
+    typename std::enable_if_t<
+        sizeof(To) == sizeof(From) &&
+        std::is_trivially_copyable_v<From> &&
+        std::is_trivially_copyable_v<To>,
+        To>
+    // constexpr support needs compiler magic
+    bit_cast(const From& src) noexcept
+    {
+        static_assert(std::is_trivially_constructible_v<To>,
+            "This implementation additionally requires destination type to be trivially constructible");    To dst;
+        std::memcpy(&dst, &src, sizeof(To));
+        return dst;
+    }
+
+} // namespace std
+
+#endif // #if __cplusplus <= 201703L
+
+#endif // #ifndef UITSL_POLYFILL_BIT_CAST_HPP_INCLUDE
