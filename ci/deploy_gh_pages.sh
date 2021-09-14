df93f62eff20147a85f07b3a88de90ecb0eedf32
mmore500 <mmore500.login@gmail.com>
2021-09-14 22:21:45 +0000

    Deploying to binder from @ mmore500/conduit@3ad8fb804f64ec017a11d64683fee390d6aa58e0 🚀

--- ci/deploy_gh_pages.sh
+++ ci/deploy_gh_pages.sh
@@ -4,7 +4,7 @@ git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis CI"
 cd web
 git init
-git remote add origin https://${GH_TOKEN}@github.com/mmore500/conduit.git > /dev/null 2>&1
+git remote add origin "https://${GH_TOKEN}@github.com/mmore500/conduit.git" > /dev/null 2>&1
 git checkout -b gh-pages
 git add .
 git commit --message "Travis build ${TRAVIS_BUILD_NUMBER}"
--- ci/fix_missing_newlines.sh
+++ ci/fix_missing_newlines.sh
@@ -4,7 +4,7 @@
 # adapted from https://stackoverflow.com/a/29689345
 TARGETS=$(find . -type f ! -path "./third-party/*" ! -path "./.git/*" ! -path "*/assets/*" ! -path "*.tar.gz" ! -path "*.jpg" ! -path "*.png" ! -path "*Makefile" ! -path "*Maketemplate*" ! -path "./node_modules/*" -exec grep -qI . {} ';' -print)
 
-for filename in $TARGETS
+for filename in ${TARGETS}
 do
-  sed -i -e '$a\' $filename
+  sed -i -e '$a\' "${filename}"
 done
