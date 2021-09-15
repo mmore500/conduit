d4e0627966eae8e88e0cd5e4057834d83e1bd661
mmore500 <mmore500@users.noreply.github.com>
2021-09-15 02:49:47 +0000

    Deploying to binder from @ mmore500/conduit@02d14e833e1d8638a8adddc961482fd36af831a6 🚀

--- conduitpylib/utils/consolidate_merge.py
+++ conduitpylib/utils/consolidate_merge.py
@@ -7,7 +7,6 @@ from .strip_end import strip_end
 def consolidate_merge(*args, **kwargs):
     """Drop-in wrapper around pandas.merge that performs an outer merge,
     then consolidates identical columns from the left and right dataframes."""
-
     assert 'how' in kwargs and kwargs['how'] == 'outer'
 
     l_suffix = '__TRYCONSOLIDATEME_LEFT__'
