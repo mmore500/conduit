1ebabdbe27d75181084fc565530f4f4c546254dd
mmore500 <mmore500.login@gmail.com>
2021-09-15 01:14:23 +0000

    Deploying to binder from @ mmore500/conduit@3ad8fb804f64ec017a11d64683fee390d6aa58e0 🚀

--- conduitpylib/README.md
+++
@@ -1,2 +0,0 @@
-Library of Python code used throughout scripts.
-Python package requirements, listed in `third-party/requirements.txt`, are pre-installed in the project's Docker container.
--- conduitpylib/utils/__init__.py
+++
@@ -1,8 +0,0 @@
-from .consolidate_merge import consolidate_merge
-from .strip_end import strip_end
-
-# adapted from https://stackoverflow.com/a/31079085
-__all__ = [
-    'consolidate_merge',
-    'strip_end',
-]
