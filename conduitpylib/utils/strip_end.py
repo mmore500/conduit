6b7bd8a5cca0b0011e6e9d746d5e326609af55e9
mmore500 <mmore500@users.noreply.github.com>
2021-09-15 01:48:17 +0000

    Deploying to binder from @ mmore500/conduit@dbf43668fd114551896d908c04dcf16cc085061b 🚀

---
+++ conduitpylib/utils/strip_end.py
@@ -0,0 +1,9 @@
+"""Contains strip_end."""
+
+# adapted from https://stackoverflow.com/a/1038999
+def strip_end(text, suffix):
+    """Strips suffix from string.
+    Essentially, polyfill for removesuffix in Python 3.9."""
+    if suffix and text.endswith(suffix):
+        return text[:-len(suffix)]
+    return text
