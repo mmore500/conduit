26993f7004bdf72dbecc60f9d9753341dcb1c98b
mmore500.login+git@gmail.com <mmore500.login+git@gmail.com>
2021-09-26 01:07:29 -0400

    Fix non-two-space indentation

--- include/uitsl/utility/exec_utils.hpp
+++ include/uitsl/utility/exec_utils.hpp
@@ -15,16 +15,16 @@ namespace uitsl {
 
 // adapted from https://stackoverflow.com/a/478960
 inline std::string exec(const char* cmd) {
-    emp::array<char, 128> buffer;
-    std::string result;
-    std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
-    if (!pipe) {
-        throw std::runtime_error("popen() failed!");
-    }
-    while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
-        result += buffer.data();
-    }
-    return result;
+  emp::array<char, 128> buffer;
+  std::string result;
+  std::unique_ptr<FILE, decltype(&pclose)> pipe(popen(cmd, "r"), pclose);
+  if (!pipe) {
+    throw std::runtime_error("popen() failed!");
+  }
+  while (fgets(buffer.data(), buffer.size(), pipe.get()) != nullptr) {
+    result += buffer.data();
+  }
+  return result;
 }
 
 } // namespace uitsl
