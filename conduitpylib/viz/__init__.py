a5975eb5b9c35e21a65d0984a6a2be53552a8119
Matthew Andres Moreno <mmore500.login+gpg@gmail.com>
2023-11-05 13:29:50 -0500

    Fix bad __all__ in __init__

--- conduitpylib/viz/__init__.py
+++ conduitpylib/viz/__init__.py
@@ -7,7 +7,7 @@ from ._set_kde_lims import set_kde_lims
 
 # adapted from https://stackoverflow.com/a/31079085
 __all__ = [
-    '_frame_scatter_subsets',
-    "_performance_semantics_plot",
-    "_set_kde_lims",
+    "frame_scatter_subsets",
+    "performance_semantics_plot",
+    "set_kde_lims",
 ]
